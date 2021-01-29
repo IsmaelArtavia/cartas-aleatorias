c0d55374e9c46815a36440d7bcdde6b24b744a2b
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-01-29 07:50:02 +0000

    ultima actualizacion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 
-var Arraynumeros = ['A',2,3,4,5,6,7,8,9,10,'J','Q','k'];
+var Arraynumeros = ['A',2,3,4,5,6,7,8,9,10,"J",'Q','k'];
 //                    corazon   espada     trebol   diamante
 var Arraysimbolos = ['&#x2665','&#x2660','&#x2663','&#x2666'];
 
@@ -11,11 +11,15 @@ var simbolos = Math.floor(Math.random()*(3)+1);
 //console.log(Arraynumeros= [numeros]);
 
 //let sign = window.prompt("cuadro");
- 
-document.querySelector("div1").innerHTML = JSON.stringify(Arraysimbolos[simbolos])
-document.querySelector("div2").innerHTML = JSON.stringify(Arraynumeros[numeros])
-document.querySelector("div3").innerHTML = JSON.stringify(Arraysimbolos[simbolos])
+  //falta poner colores 
+  
 
+document.querySelector("div1").innerHTML = JSON.stringify(Arraysimbolos[simbolos])
+document.querySelector("div3").innerHTML = JSON.stringify(Arraysimbolos[simbolos])
+document.querySelector("div2").innerHTML = JSON.stringify(Arraynumeros[numeros])
+
+
+  
 //var img = new Image();
 //img.src = "fas fa-fighter-jet";
 //<i class="fas fa-fighter-jet"></i>
