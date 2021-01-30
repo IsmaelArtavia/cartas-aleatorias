e032eda9df30e068b2162d6ebbf5fc66b38d6d53
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-01-30 02:12:27 +0000

    cartas aleatorias

--- index.js
+++ index.js
@@ -3,23 +3,30 @@
 
 var Arraynumeros = ['A',2,3,4,5,6,7,8,9,10,"J",'Q','k'];
 //                    corazon   espada     trebol   diamante
-var Arraysimbolos = ['&#x2665','&#x2660','&#x2663','&#x2666'];
+var Arraysimbolos = ["&#x2665",'\u2660','&#x2663','&#x2666'];
 
-var numeros = Math.floor(Math.random()*(12)+1);
-var simbolos = Math.floor(Math.random()*(3)+1);
+var numeros = Math.floor(Math.random()*(12));
+var simbolos = Math.floor(Math.random()*(4));
 
 //console.log(Arraynumeros= [numeros]);
 
 //let sign = window.prompt("cuadro");
   //falta poner colores 
   
+if(Arraysimbolos[simbolos]==Arraysimbolos[0] || Arraysimbolos[simbolos]==Arraysimbolos[3]){
+    let div1 = document.querySelector('div1'); 
+    let div3 = document.querySelector('div3'); 
+    div1.style.color = 'red';
+    div3.style.color = 'red';
+} 
 
-document.querySelector("div1").innerHTML = JSON.stringify(Arraysimbolos[simbolos])
-document.querySelector("div3").innerHTML = JSON.stringify(Arraysimbolos[simbolos])
-document.querySelector("div2").innerHTML = JSON.stringify(Arraynumeros[numeros])
-
+document.querySelector("div1").innerHTML = Arraysimbolos[simbolos]
+document.querySelector("div2").innerHTML = Arraynumeros[numeros]
+document.querySelector("div3").innerHTML = Arraysimbolos[simbolos]
 
   
 //var img = new Image();
 //img.src = "fas fa-fighter-jet";
 //<i class="fas fa-fighter-jet"></i>
+
+
